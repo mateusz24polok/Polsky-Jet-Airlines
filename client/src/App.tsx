33b0c8512e2eff805b4ac2bd998abf15855b30b2
Mateusz Polok <mateusz24polok@gmail.com>
2021-04-20 22:43:16 +0200

    Trial commit to check lint-staged correctly work v2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,8 @@ const App: React.FC = () => {
   const title = "Main component of POLSKY JET APP";
   return (
     <header>
-      <h1>
-        {title} - {process.env.name}
-      </h1>
+      <h1>{title}</h1>
+      <h2>{process.env.name}</h2>
     </header>
   );
 };
