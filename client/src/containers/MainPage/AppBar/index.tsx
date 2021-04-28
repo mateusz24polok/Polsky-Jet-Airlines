468f4f333530d477eae73b5175048fb775563af0
Mateusz Polok <mateusz24polok@gmail.com>
2021-04-28 22:14:09 +0200

    Extend media querries utils of up and down perspective

--- client/src/containers/MainPage/AppBar/index.tsx
+++ client/src/containers/MainPage/AppBar/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { DesktopNavBar } from "@containers/mainPage/AppBar/DekstopNavBar";
 import { MobileNavBar } from "@containers/mainPage/AppBar/MobileNavBar";
-import { useMediumBrekpointMatches } from "@utils/mediaQuerriesUtils";
+import { useMediumBrekpointMatchesUp } from "@utils/mediaQuerriesUtils";
 
 export const AppBar: React.FC = () => {
-  const mediumMatches = useMediumBrekpointMatches();
+  const mediumMatches = useMediumBrekpointMatchesUp();
 
   return <>{mediumMatches ? <DesktopNavBar /> : <MobileNavBar />}</>;
 };
