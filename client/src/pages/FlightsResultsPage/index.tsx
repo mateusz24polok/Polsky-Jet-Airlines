d8109f5e0216557e2d6d6ed7206006bfeb1f360d
Mateusz Polok <mateusz24polok@gmail.com>
2021-05-16 22:41:49 +0200

    Adjust RWD in FlightListContainer

--- client/src/pages/FlightsResultsPage/index.tsx
+++ client/src/pages/FlightsResultsPage/index.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
-import { Typography } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import {
   fetchFlights,
   selectFlights,
   selectIsProgress,
 } from "@store/slices/flights";
 import { prepareObjectFromQueryParamsURL } from "@utils/urlUtils";
+import { useMediumBrekpointMatchesUp } from "@utils/mediaQuerriesUtils";
 import { FlightsSearchFilters } from "@appTypes/flight";
 import { FlightListContainer } from "@containers/FlightListContainer";
 import { FlightListItem } from "@components/flight/FlightListItem";
@@ -17,6 +18,7 @@ export const FlightsResultPage: React.FC = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const location = useLocation();
+  const mediumMediaBreakpointMatches = useMediumBrekpointMatchesUp();
   const flightsSearchFiltersFromURLQueryParams = prepareObjectFromQueryParamsURL(
     location.search,
   );
@@ -34,7 +36,19 @@ export const FlightsResultPage: React.FC = () => {
   }, [dispatch, location]);
 
   const renderFlightsList = (): JSX.Element => (
-    <>
+    <Grid
+      container
+      justify="space-around"
+      direction={mediumMediaBreakpointMatches ? "column" : "row"}
+    >
+      <FlightListItem />
+      <FlightListItem />
+      <FlightListItem />
+      <FlightListItem />
+      <FlightListItem />
+      <FlightListItem />
+      <FlightListItem />
+      <FlightListItem />
       <FlightListItem />
       {/* {flights.map(flight => (
         <li key={flight._id}>
@@ -42,7 +56,7 @@ export const FlightsResultPage: React.FC = () => {
           {flight.startingDate}
         </li>
       ))} */}
-    </>
+    </Grid>
   );
 
   const renderFallbackView = (): JSX.Element => <h2>Brak wyników</h2>;
