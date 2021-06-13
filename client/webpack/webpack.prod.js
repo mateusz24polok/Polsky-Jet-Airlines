3d5ff95670f8f4dcb149a0e7780099910f3ab56d
Mateusz Polok <mateusz24polok@gmail.com>
2021-06-13 23:08:49 +0200

    improve html file with og:image property and favicon

--- client/webpack/webpack.prod.js
+++ client/webpack/webpack.prod.js
@@ -8,9 +8,9 @@ module.exports = {
     new webpack.DefinePlugin({
       "process.env.name": JSON.stringify("Polsky Jet Production App"),
       "process.env.api": JSON.stringify("https://polsky-jet.herokuapp.com"),
-    }),
-    new Dotenv({
-      path: "./.env",
+      "process.env.OPEN_WEATHER_API_KEY:": JSON.stringify(
+        "9846a286c520f631c0095b92d0ac8d0e",
+      ),
     }),
   ],
 };
