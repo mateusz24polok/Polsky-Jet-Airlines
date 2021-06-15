71520e8eb3e0ec010fd0bd3d8d254baadea81f4f
Mateusz Polok <mateusz24polok@gmail.com>
2021-06-15 23:55:15 +0200

    based initial loggedIn state on jwt from localstorage which fixed refresh logout issue

--- client/src/utils/authUtils.ts
+++ client/src/utils/authUtils.ts
@@ -1,3 +1,6 @@
+import jwt_decode from "jwt-decode";
+import { Jwt } from "@appTypes/shared/jwt";
+
 export const setJwtInLocalStorage = (jwt: string): void => {
   if (window.localStorage) {
     localStorage.setItem("jwt", jwt);
@@ -14,4 +17,23 @@ export const removeJwtFromLocalStorage = (): void => {
   if (window.localStorage && localStorage.getItem("jwt")) {
     localStorage.removeItem("jwt");
   }
+};
+
+export const getUserIdFromJwt = (jwt: string): string | null => {
+  if (jwt && jwt !== "") {
+    const decodedToken = jwt_decode<Jwt>(jwt);
+    return decodedToken.id;
+  }
+  return null;
+};
+
+export const getTokenValidity = (jwt: string) => {
+  if (jwt && jwt !== "") {
+    const decodedToken = jwt_decode<Jwt>(jwt);
+    const tokenExpirationDate = decodedToken.exp;
+    return Boolean(
+      tokenExpirationDate && tokenExpirationDate * 1000 > new Date().getTime(),
+    );
+  }
+  return false;
 };
