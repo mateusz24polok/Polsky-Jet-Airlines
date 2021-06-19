7fa8e0bd7df9e233bd6edee884c01915cab51371
Mateusz Polok <mateusz24polok@gmail.com>
2021-06-19 20:51:16 +0200

    get access to the managment sites only for not-mobile devices

--- client/src/pages/ManagementPage/index.tsx
+++ client/src/pages/ManagementPage/index.tsx
@@ -4,35 +4,44 @@ import { Grid } from "@material-ui/core";
 import { SideMenu } from "@components/management/SideMenu";
 import { routes } from "@resources/res.routes";
 import { getManagementRoutes } from "@utils/routesUtils";
+import { useMediumBrekpointMatchesUp } from "@utils/mediaQuerriesUtils";
+import { ManagementNotAvailableOnMobileDevicesPage } from "./NotAvailableOnMobileDevicesPage";
 import { useStyles } from "./styles";
 
 export const ManagementPage: React.FC = () => {
   const classes = useStyles();
+  const mediumMediaBreakpointMatches = useMediumBrekpointMatchesUp();
   return (
-    <Grid
-      className={classes.container}
-      container
-      alignItems="stretch"
-      wrap="nowrap"
-    >
-      <Grid className={classes.menu} item xs={2}>
-        <SideMenu />
-      </Grid>
-      <Grid className={classes.dataContent} item xs={10}>
-        <Switch>
-          {getManagementRoutes(routes).map(managementRoute => (
-            <Route
-              key={managementRoute.id}
-              exact={managementRoute.exact}
-              path={managementRoute.path}
-              component={managementRoute.component as React.FC<unknown>}
-            />
-          ))}
-          <Route>
-            <h1>{`Management site you're trying to reach doesn't exist`}</h1>
-          </Route>
-        </Switch>
-      </Grid>
-    </Grid>
+    <>
+      {mediumMediaBreakpointMatches ? (
+        <Grid
+          className={classes.container}
+          container
+          alignItems="stretch"
+          wrap="nowrap"
+        >
+          <Grid className={classes.menu} item xs={2}>
+            <SideMenu />
+          </Grid>
+          <Grid className={classes.dataContent} item xs={10}>
+            <Switch>
+              {getManagementRoutes(routes).map(managementRoute => (
+                <Route
+                  key={managementRoute.id}
+                  exact={managementRoute.exact}
+                  path={managementRoute.path}
+                  component={managementRoute.component as React.FC<unknown>}
+                />
+              ))}
+              <Route>
+                <h1>{`Management site you're trying to reach doesn't exist`}</h1>
+              </Route>
+            </Switch>
+          </Grid>
+        </Grid>
+      ) : (
+        <ManagementNotAvailableOnMobileDevicesPage />
+      )}
+    </>
   );
 };
